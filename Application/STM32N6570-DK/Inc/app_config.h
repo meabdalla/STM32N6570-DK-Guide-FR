eeac0ba50b597b95ebac00427415363736db6123
Abdalla <gagatrozz@gmail.com>
2025-12-03 18:42:55 +0100

    Cleanup workspace: removed NUCLEO files, kept STM32N6 DK only. Updated app_config.h to support 80 classes.

--- Application/STM32N6570-DK/Inc/app_config.h
+++ Application/STM32N6570-DK/Inc/app_config.h
@@ -43,13 +43,20 @@
 #define COLOR_MODE    COLOR_RGB
 
 /* Classes */
-#define NB_CLASSES   (1)
+#define NB_CLASSES   (80)
 #define CLASSES_TABLE const char* classes_table[NB_CLASSES] = {\
-   "person"}\
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",\
+    "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",\
+    "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",\
+    "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",\
+    "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange",\
+    "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed",\
+    "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven",\
+    "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"}\
 
 /* I/O configuration */
 /* Postprocessing ST_YOLO_X configuration */
-#define AI_OD_ST_YOLOX_PP_NB_CLASSES        (1)
+#define AI_OD_ST_YOLOX_PP_NB_CLASSES        (80)
 #define AI_OD_ST_YOLOX_PP_L_GRID_WIDTH      (60)
 #define AI_OD_ST_YOLOX_PP_L_GRID_HEIGHT     (60)
 #define AI_OD_ST_YOLOX_PP_L_NB_INPUT_BOXES  (AI_OD_ST_YOLOX_PP_L_GRID_WIDTH * AI_OD_ST_YOLOX_PP_L_GRID_HEIGHT)
